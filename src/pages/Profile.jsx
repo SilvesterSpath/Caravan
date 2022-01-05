672683b742b180164b4171b8e4ab72b7366d7ef8
SilvesterSpath <spath.sz@gmail.com>
2022-01-05 08:58:49 +0100

    Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import {getAuth} from 'firebase/auth'
+import {useEffect, useState} from 'react'
 
 function Profile() {
+  const [user, setUser] = useState(null)
+
+  const auth = getAuth()
+
+  useEffect(()=>{
+    setUser(auth.currentUser)
+    console.log(auth.currentUser);
+  }, [])
+
   return (
     <div>
       <h1>Profile</h1>
+      {user ? <h3>Welcome: {user.displayName}</h3> : 'Not Logged In'}
     </div>
   )
 }
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
-import { getAuth, signInWithEmailnAndPassword } from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -22,9 +22,20 @@ function SignIn() {
     }))
   }
 
-  const onSubmit = (e)=>{
+  const onSubmit = async (e)=>{
     e.preventDefault()
-    
+
+    try {
+      const auth = getAuth()
+
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)    
+
+      if(userCredential.user){
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error);
+    }   
   }
 
   return (
