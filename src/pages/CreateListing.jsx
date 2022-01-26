ecbd723225a85fbf71e7b91ae3e7fb87626e8563
SilvesterSpath <spath.sz@gmail.com>
2022-01-26 08:20:44 +0100

    form 1

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -4,7 +4,7 @@ import {useNavigate} from 'react-router-dom'
 import Spinner from '../components/Spinner'
 
 function CreateListing() {
-  const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+  const [geolocationEnabled, setGeolocationEnabled] = useState(false)
   const [loading, setLoading] = useState(false)
   const [formData, setFormData] = useState({
     type: 'rent',
@@ -21,6 +21,8 @@ function CreateListing() {
     latitude: 0,
     longitude: 0 
   })
+
+  const {type, name, bathrooms, bedrooms, trailer, furnished, address, offer, regularPrice, discountPrice, images, latitude, longitude} = formData
 
   const auth = getAuth()
   const navigate = useNavigate()
@@ -39,15 +41,91 @@ function CreateListing() {
     return ()=>{
       isMounted.current = false
     }
-    
-  }, [isMounted, auth, navigate, formData])
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps    
+  }, [ isMounted ])
+
+  const onSubmit = (e)=>{
+    e.preventDefault()
+
+  }
+
+  const onMutate = (e)=>{
+
+  }
 
   if(loading){
     return <Spinner/>
   }
 
-  return <div>
-    Create
+  return <div className='profile'>
+    <header>
+      <p className="pageHeader">Create a listing</p>
+    </header>
+    <main>
+      <form onSubmit={onSubmit}>
+        <label className='formLabel'>Sell / Rent</label>
+        <div className="formButtons">
+          <button type='button' className={type === 'sale' ? 'formButtonActive': 'formButton'}
+          id='type'
+          value='sale'
+          onClick={onMutate}
+          >
+            Sell
+          </button>
+          <button type='button' className={type === 'rent' ? 'formButtonActive': 'formButton'}
+          id='type'
+          value='rent'
+          onClick={onMutate}
+          >
+            Rent
+          </button>
+        </div>
+        <label className='formLabel'>Name</label>
+        <input className='formInputName' type="text" id='name' value={name} onChange={onMutate} maxLength='32' minLength='10' required/>
+
+        <div className="formRooms flex">
+          <div>
+            <label className='formLabel'>Bedrooms</label>
+            <input type="number" className='formInputSmall' id='bedrooms' value={bedrooms} onChange={onMutate} min='1' max='50' required />            
+          </div>
+          <div>
+          <label className='formLabel'>Bathrooms</label>
+            <input type="number" className='formInputSmall' id='bathrooms' value={bathrooms} onChange={onMutate} min='1' max='50' required />
+          </div>
+        </div>
+        <label className='formLabel'>Trailer</label>
+        <div className="formButtons">
+          <button className={trailer ? 'formButtonActive': 'formButton'} type='button' id='trailer' value={trailer} onClick={onMutate} >Yes</button>
+          <button className={!trailer ? 'formButtonActive': 'formButton'} type='button' id='trailer' value={trailer} onClick={onMutate} >No</button>
+        </div>
+        <label className='formLabel'>Furnished</label>
+        <div className="formButtons">
+          <button className={furnished ? 'formButtonActive': 'formButton'} type='button' id='furnished' value={furnished} onClick={onMutate} >Yes</button>
+          <button className={!furnished ? 'formButtonActive': 'formButton'} type='button' id='furnished' value={furnished} onClick={onMutate} >No</button>
+        </div>
+
+        <label className='formLabel'>Address</label>
+        <textarea className="formInputAddress" type='text' id="address" value={address} onChange={onMutate} required />
+
+        {!geolocationEnabled && (
+          <div className="formLatLng flex">
+            <div>
+              <label className='formLabel'>Latitude</label>
+              <input type="number" className='formInputSmall' id='latitude' value={latitude} onChange={onMutate} required />
+            </div>
+            <div>
+              <label className='formLabel'>Longitude</label>
+              <input type="number" className='formInputSmall' id='longitude' value={longitude} onChange={onMutate} required />
+            </div>
+          </div>
+        )}
+
+        
+        
+      </form>
+    </main>
+    
   </div>;
 }
 
